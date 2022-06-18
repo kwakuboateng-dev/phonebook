25ebf38db5471df4abd9d8b3215d563c4b063c59
Kwaku Boateng <kwakuboateng.dev@outlook.com>
2022-06-18 02:07:43 +0000

    form validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  validates :first_name, presence: true, length: {minimum: 3, maximum: 20 }
+  
+  validates :phone_number, presence: true, length: {minimum: 10}
 end
